633145eed79e8346f158fc171961e1e4b76cbd0e
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-08-07 16:52:30 +0200

    added jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
             <div class="row">
                 @foreach ($merch as $m)
                     <div class="col">
-                        <img src="{{ asset(images / $m['image']) }}" :alt="{{ $m['title'] }}">
+                        <img src="{{ asset("images/$m[image]") }}" alt="">
                         <h2>{{ $m['title'] }}</h2>
                     </div>
                 @endforeach
